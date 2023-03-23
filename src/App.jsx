13f94a732d8c1d3d9fd0c7943ba86bf726a7ab8c
swz22 <jkim005@yahoo.com>
2023-03-23 16:36:05 -0500

    create searchbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Sidebar } from "./components";
+import { Sidebar, Searchbar } from "./components";
 
 const App = () => (
   <div className="flex">
     <Sidebar />
-    <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286] sm:p-12 p-6">
+    <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+      <Searchbar />
       Placeholder
     </div>
   </div>
