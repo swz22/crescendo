d759bdeb283ffa90dfaca55b6896645e9f010517
swz22 <jkim005@yahoo.com>
2023-03-24 10:28:38 -0500

    create songcard component and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Sidebar, Searchbar } from "./components";
+import { Home } from "./pages";
 
 const App = () => (
   <div className="flex">
     <Sidebar />
     <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
       <Searchbar />
-      Placeholder
+      <div className="p-6 h-[93vh] overflow-y-scroll hide-scrollbar">
+        <Home />
+      </div>
     </div>
   </div>
 );
