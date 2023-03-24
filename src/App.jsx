0d914a3ff43ef544463f4bd89edbea8590e54615
swz22 <jkim005@yahoo.com>
2023-03-24 10:40:29 -0500

    create top charts component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Sidebar, Searchbar } from "./components";
+import { Sidebar, Searchbar, TopPlay } from "./components";
 import { Home } from "./pages";
 
 const App = () => (
@@ -6,8 +6,11 @@ const App = () => (
     <Sidebar />
     <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
       <Searchbar />
-      <div className="p-6 h-[93vh] overflow-y-scroll hide-scrollbar">
-        <Home />
+      <div className="p-6 flex flex-row">
+        <div className="flex-1 h-[90vh] pb-10 overflow-y-scroll hide-scrollbar">
+          <Home />
+        </div>
+        <TopPlay />
       </div>
     </div>
   </div>
