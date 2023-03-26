cdb50ee1f4890d0e73280a2bfa7440a5f64ffae1
John Kim <jkim005@yahoo.com>
2023-03-25 21:17:58 -0500

    implement top charts and search

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { Searchbar, Sidebar, TopPlay } from "./components";
 import { Route, Routes } from "react-router-dom";
-import { Artists, ArtistDetails, Home, SongDetails } from "./pages";
+import { Searchbar, Sidebar, TopPlay } from "./components";
+import {
+  ArtistDetails,
+  Artists,
+  Home,
+  Search,
+  SongDetails,
+  TopCharts,
+} from "./pages";
 
 const App = () => (
   <div className="flex">
     <Sidebar />
     <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
       <Searchbar />
+
       <div className="px-6 flex xl:flex-row flex-col-reverse">
         <div className="flex-1 pb-10 h-[90vh] overflow-y-scroll hide-scrollbar">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/artists" element={<Artists />} />
+            <Route path="/top-charts" element={<TopCharts />} />
             <Route path="/artists/:id" element={<ArtistDetails />} />
             <Route path="/songs/:songid" element={<SongDetails />} />
+            <Route path="/search/:searchTerm" element={<Search />} />
           </Routes>
         </div>
         <TopPlay />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import Home from "./Home";
 import Artists from "./Artists";
 import ArtistDetails from './ArtistDetails';
+import Search from './Search';
 import SongDetails from './SongDetails';
+import TopCharts from './TopCharts';
 
-export { Home, Artists, ArtistDetails, SongDetails };
+export { Home, Artists, ArtistDetails, Search, SongDetails, TopCharts };
