219d040bfce917c875607553d6054df28f014567
swz22 <jkim005@yahoo.com>
2023-03-27 11:19:52 -0500

    add fixes to ui and package.json

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -2,8 +2,7 @@ import { Link } from "react-router-dom";
 
 const DetailsHeader = ({ artistId, artistData, songData }) => (
   <div className="relative w-full flex flex-col">
-    <div className="w-full bg-gradient-to-l from-transparent to-black h-48" />
-
+    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
     <div className="absolute inset-0 flex items-center">
       <img
         alt="profile"
@@ -12,19 +11,18 @@ const DetailsHeader = ({ artistId, artistData, songData }) => (
             ? artistData?.artists[artistId].attributes?.artwork?.url
                 .replace("{w}", "500")
                 .replace("{h}", "500")
-            : songData?.images.coverart
+            : songData?.images?.coverart
         }
-        className="w-48 h-48 rounded-full object-cover border-2 shadow-xl shadow-black"
+        className="sm:w-48 w-28 sm:h-48 w-28 rounded-full object-cover border-2 shadow-xl shadow-black"
       />
-
       <div className="ml-5">
-        <p className="font-bold text-3xl text-white">
+        <p className="font-bold sm:text-3xl text-xl text-white">
           {artistId
-            ? artistData?.artists[artistId].attributes.name
+            ? artistData?.artists[artistId].attributes?.name
             : songData?.title}
         </p>
         {!artistId && (
-          <Link to={`/artists/${songData?.artists[0].adamid}`}>
+          <Link to={`/artists/${songData?.artists[0]?.adamid}`}>
             <p className="text-base text-gray-400 mt-2">{songData?.subtitle}</p>
           </Link>
         )}
@@ -36,8 +34,7 @@ const DetailsHeader = ({ artistId, artistData, songData }) => (
         </p>
       </div>
     </div>
-
-    <div className="w-full h-44" />
+    <div className="w-full sm:h-44 h-24" />
   </div>
 );
 
