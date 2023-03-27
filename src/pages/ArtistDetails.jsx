254b3a7fd796f1f6cc7adf49a413a799882d4163
swz22 <jkim005@yahoo.com>
2023-03-27 11:55:48 -0500

    add more fixes and rename files

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,53 +1,30 @@
 import { useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
-import {
-  useFetchArtistDetailsQuery,
-  useGetSongDetailsQuery,
-} from "../redux/services/shazamCore";
-import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { useGetArtistDetailsQuery } from "../redux/services/shazamCore";
 
 const ArtistDetails = () => {
-  const dispatch = useDispatch();
-  const { songid, id: artistId } = useParams();
+  const { id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data: songData, isFetching: isFetchinSongDetails } =
-    useGetSongDetailsQuery({ songid });
   const {
     data: artistData,
     isFetching: isFetchingArtistDetails,
     error,
-  } = useFetchArtistDetailsQuery(artistId);
+  } = useGetArtistDetailsQuery(artistId);
 
-  if (isFetchinSongDetails || isFetchingArtistDetails)
+  if (isFetchingArtistDetails)
     return <Loader title="Loading artist details..." />;
 
   if (error) return <Error />;
 
-  const handlePauseClick = () => {
-    dispatch(playPause(false));
-  };
-
-  const handlePlayClick = (song, i) => {
-    dispatch(setActiveSong({ song, songData, i }));
-    dispatch(playPause(true));
-  };
-
   return (
     <div className="flex flex-col">
-      <DetailsHeader
-        artistId={artistId}
-        artistData={artistData}
-        songData={songData}
-      />
-
+      <DetailsHeader artistId={artistId} artistData={artistData} />
       <RelatedSongs
         data={Object.values(artistData?.songs)}
         artistId={artistId}
         isPlaying={isPlaying}
         activeSong={activeSong}
-        handlePauseClick={handlePauseClick}
-        handlePlayClick={handlePlayClick}
       />
     </div>
   );
