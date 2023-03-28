3f1f0aedc27b9c002b33720e5ddbf9f6a646e8d0
John Kim <jkim005@yahoo.com>
2023-03-28 18:55:58 -0500

    update styling

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ const App = () => {
   return (
     <div className="relative flex">
       <Sidebar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+      <div className="flex-1 flex flex-col bg-gradient-to-r from-[#110d36] via-[#352f73] to-[#110d36]">
+        //via-[#302b63]
         <Searchbar />
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
