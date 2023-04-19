decaf02809e18cba6f52b599e7a0499c2ba1d278
John Kim <jkim005@yahoo.com>
2023-04-19 17:57:06 -0500

    fix bug with api

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -12,18 +12,13 @@ const CountryTracks = () => {
 
   useEffect(() => {
     axios
-      .get(
-        `https://geo.ipify.org/api/v2/country?apiKey=${
-          import.meta.env.VITE_GEO_API_KEY
-        }`
-      )
+      .get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`)
       .then((res) => setCountry(res?.data?.location.country))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
   }, [country]);
 
-  if (isFetching && loading)
-    return <Loader title="Loading Songs around you..." />;
+  if (isFetching && loading) return <Loader title="Loading Songs around you..." />;
 
   if (error && country !== "") return <Error />;
 
