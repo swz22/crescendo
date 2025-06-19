4f3763d4784d57f785bf4ccf10658f419df2291b
John Kim <jkdev220@gmail.com>
2025-06-19 16:31:57 -0500

    refactor: remove unused layout context

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-
 import "./index.css";
 import App from "./App";
 import { store } from "./redux/store";
 import { ToastProvider } from "./context/ToastContext";
-import { LayoutProvider } from "./context/LayoutContext";
 import { AppProvider } from "./context/AppContext";
 import ErrorBoundary from "./components/ErrorBoundary";
 
@@ -15,15 +13,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <AppProvider>
-        <LayoutProvider>
-          <ToastProvider>
-            <Router>
-              <ErrorBoundary>
-                <App />
-              </ErrorBoundary>
-            </Router>
-          </ToastProvider>
-        </LayoutProvider>
+        <ToastProvider>
+          <Router>
+            <ErrorBoundary>
+              <App />
+            </ErrorBoundary>
+          </Router>
+        </ToastProvider>
       </AppProvider>
     </Provider>
   </React.StrictMode>
